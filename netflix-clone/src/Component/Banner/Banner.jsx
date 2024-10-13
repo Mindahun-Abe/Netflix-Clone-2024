0b4d1e96cabdf2db3ffcb610e00600862aac937b
Mindahun-Abe <mindahun77@gmail.com>
2024-10-13 20:30:22 +0300

    add
    dev add

---
+++ netflix-clone/src/Component/Banner/Banner.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import axios from '../Utilities/axios';
+import Requests from '../Utilities/requests';
+
+
+
+
+const Banner = () => {
+	const [movie, setMovie] = useState(());
+	useEffect(()=>{
+		(async () => {
+			try{
+const request = await axios.get(requests.fetchNetflixOriginals)
+setMovie(request.data.results[Math.floor(Math.random()*request.data.results.length)
+]);
+}catch(error){
+console.log('error', error);
+}
+		})()
+	},[]);
+
+
+	return (
+		<div className='Banner' 
+		style=({
+			backgroundSize: 'cover',
+            backImage: 'url("https://image.tmdb.org/t/p/original${movie?backdrop_path}")',
+            backgroundPosition: 'center',
+            backgroundRepeat: "no-repeat"
+
+		})
+		>
+		<div className='Banner_contents'>
+          <h1 className='Banner_title'>
+            {movie?.title || movie?.name || movie.original_name}
+           </h1>
+		   <div className='banner_buttons'>
+			<button className='banner__button play'>Play</button>
+            <button className='banner__button play'>My List</button>
+		   </div>
+		   {/* <h1 className='banner_description'>{truncate(movie?.overview. 150)}</h1> */}
+		</div>
+		<div className='banner_fadeButton'/>
+		</div>
+	);
+};
+
+
+Banner.propTypes = {
+
+};
+
+
+export default Banner;
--- netflix-clone/src/Pages/Home/Home.jsx
+++ netflix-clone/src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../../Component/Header/Header'
 import Footer from '../../Component/Footer/Footer';
+import Banner from '../../Component/Banner/Banner';
 
 
 
@@ -9,6 +10,7 @@ const Home = () => {
 	return (
 		<div>
   <Header/>
+<Banner/>
 
   <Footer/>
 
