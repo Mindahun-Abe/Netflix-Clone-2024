772046312f66227376b7dd0c3444df003362682c
Mindahun-Abe <mindahun77@gmail.com>
2024-10-14 10:13:30 +0300

    change file
    to have go results

--- netflix-clone/src/Component/Banner/Banner.jsx
+++ netflix-clone/src/Component/Banner/Banner.jsx
@@ -1,35 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import axios from '../Utilities/axios';
-import Requests from '../Utilities/requests';
+import requests from '../Utilities/requests';
 
 
 
 
 const Banner = () => {
-	const [movie, setMovie] = useState(());
-	useEffect(()=>{
-		(async () => {
-			try{
-const request = await axios.get(requests.fetchNetflixOriginals)
-setMovie(request.data.results[Math.floor(Math.random()*request.data.results.length)
-]);
-}catch(error){
-console.log('error', error);
-}
-		})()
-	},[]);
+	const [movie, setMovie] = useState([]);
+	useEffect(() => {
+		fetchmovie();
+	  }, []);
+	
+	  const fetchmovie = async () => {
+		try {
+		  const request = await axios.get('requests.fetchNetflixOriginals');
+		  setmovie(request.data.results[Math.floor(Math.random()*request.data.results.length)
+		  ]);
+		} catch (error) {
+		  console.log('Error fetching movie:', error);
+		}
+	  };  
 
 
 	return (
 		<div className='Banner' 
-		style=({
+		style={{
 			backgroundSize: 'cover',
             backImage: 'url("https://image.tmdb.org/t/p/original${movie?backdrop_path}")',
-            backgroundPosition: 'center',
-            backgroundRepeat: "no-repeat"
-
-		})
+			backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat'
+		}}
 		>
 		<div className='Banner_contents'>
           <h1 className='Banner_title'>
